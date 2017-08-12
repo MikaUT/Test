41a6ef63aec0e9fb22064b8e95504c583c448514
Mika Thompson <musher42@gmail.com>
2017-08-12 11:27:49 -0700

    Initial Commit

---
+++ Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by Mika Usher on 8/12/17.
+//  Copyright © 2017 usherm42@uw.edu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
